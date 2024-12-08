9a998a94241916450d836630e9ce23ca886f53bf
Brian Blosser <bpb54321@gmail.com>
2024-12-08 15:38:51 -0500

    Add an assetReportCreate call to AccountPage.

--- app/accounts/[accountId]/page.tsx
+++ app/accounts/[accountId]/page.tsx
@@ -1,7 +1,49 @@
+import {
+  AssetReportCreateRequest,
+  Configuration,
+  PlaidApi,
+  PlaidEnvironments,
+} from "plaid";
+
 interface AccountPageProps {
   params: Promise<{ accountId: string }>;
+  accessToken: string;
 }
-export default async function AccountPage({ params }: AccountPageProps) {
+export default async function AccountPage({
+  params,
+  accessToken,
+}: AccountPageProps) {
   const { accountId } = await params;
-  return <h1>Account Page for Account {accountId}</h1>;
+
+  // TODO: Move the plaidClient to Context and reuse the same plaidClient.
+  const configuration = new Configuration({
+    basePath: PlaidEnvironments.production,
+    baseOptions: {
+      headers: {
+        "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+        "PLAID-SECRET": process.env.PLAID_SECRET,
+        "Plaid-Version": "2020-09-14",
+      },
+    },
+  });
+
+  const plaidClient = new PlaidApi(configuration);
+
+  const daysRequested = 90;
+  const request: AssetReportCreateRequest = {
+    access_tokens: [accessToken],
+    days_requested: daysRequested,
+  };
+
+  const response = await plaidClient.assetReportCreate(request);
+  const assetReportId = response.data.asset_report_id;
+  const assetReportToken = response.data.asset_report_token;
+
+  return (
+    <section>
+      <h1>Account Page for Account {accountId}</h1>
+      <p>{assetReportId}</p>
+      <p>{assetReportToken}</p>
+    </section>
+  );
 }
