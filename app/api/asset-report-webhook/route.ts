53d0c6b763bf009b8f36572db336e00188ddeefd
Brian Blosser <bpb54321@gmail.com>
2024-12-14 15:07:36 -0500

    Set up server-side event for the webhook.

--- app/api/asset-report-webhook/route.ts
+++ app/api/asset-report-webhook/route.ts
@@ -1,7 +1,42 @@
+let clients: {
+  id: string;
+  controller: ReadableStreamDefaultController;
+}[] = [];
+
 export async function POST(request: Request) {
-  const requestBody = await request.json();
-  console.log(requestBody);
-  return Response.json({
-    message: "Thank you for calling the asset-report-webhook route!",
+  const webhookBody = await request.json();
+
+  console.log(webhookBody);
+  // Send the data to all connected clients
+  clients.forEach((client) => {
+    client.controller.enqueue(`data: ${JSON.stringify(webhookBody)}\n\n`);
   });
+
+  return Response.json({
+    status: "Ok",
+  });
+}
+
+export async function GET() {
+  const clientId = Date.now().toString();
+
+  const stream = new ReadableStream({
+    start(controller) {
+      controller.enqueue(`data: Connected to webhook stream\n\n`);
+
+      clients.push({ controller, id: clientId });
+    },
+    cancel(reason) {
+      // Callback function when the connection is closed
+      clients = clients.filter((client) => client.id !== clientId);
+      console.log(`Connection closed: ${reason}`);
+    },
+  });
+
+  const response = new Response(stream);
+  response.headers.append("Connection", "keep-alive");
+  response.headers.append("Cache-Control", "no-cache");
+  response.headers.append("Content-Type", "text/event-stream");
+
+  return response;
 }
