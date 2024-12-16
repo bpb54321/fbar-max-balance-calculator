b16c3ca2011cac2b4cec4b62b2d8217c6f44a9f9
Brian Blosser <bpb54321@gmail.com>
2024-12-16 16:49:53 -0500

    Add feedback for asset report generation.

--- components/AssetReportCreationButton.tsx
+++ components/AssetReportCreationButton.tsx
@@ -6,6 +6,16 @@ import createAssetReport from "@/server-functions/createAssetReport";
 import getAssetReport from "@/server-functions/getAssetReport";
 import { useEffect, useState } from "react";
 
+enum AssetReportCreationState {
+  Idle,
+  CreationRequestInitiated,
+  CreationRequestConfirmed,
+  ReportReady,
+  ErrorGeneratingReport,
+  ReportFetchInitiated,
+  ReportFetchCompleted,
+}
+
 export default function AssetReportCreationButton({
   setAssetReport,
   plaidItem,
@@ -13,6 +23,8 @@ export default function AssetReportCreationButton({
   setAssetReport: (assetReport: AssetReport) => void;
   plaidItem: PlaidItem;
 }) {
+  const [assetReportCreationState, setAssetReportCreationState] =
+    useState<AssetReportCreationState>(AssetReportCreationState.Idle);
   const [assetReportCreateResponseState, setAssetReportCreateResponse] =
     useState<AssetReportCreateResponse>({
       asset_report_id: "",
@@ -39,9 +51,18 @@ export default function AssetReportCreationButton({
       if (assetsWebhookPayload.error) {
         console.error("Received an error webhook");
         console.error(assetsWebhookPayload);
+        setAssetReportCreationState(
+          AssetReportCreationState.ErrorGeneratingReport
+        );
       } else {
+        setAssetReportCreationState(
+          AssetReportCreationState.ReportFetchInitiated
+        );
         const assetReport = await getAssetReport(
           assetReportCreateResponseState.asset_report_token
+        );
+        setAssetReportCreationState(
+          AssetReportCreationState.ReportFetchCompleted
         );
         setAssetReport(assetReport);
       }
@@ -63,14 +84,48 @@ export default function AssetReportCreationButton({
     };
   }, [assetReportCreateResponseState, setAssetReport]);
 
+  let reportCreationProgressFeedbackText;
+  switch (assetReportCreationState) {
+    case AssetReportCreationState.CreationRequestInitiated:
+      reportCreationProgressFeedbackText =
+        "Sending request to create asset report...";
+      break;
+    case AssetReportCreationState.CreationRequestConfirmed:
+      reportCreationProgressFeedbackText =
+        "Request received. Generating asset report. This could take up to a few minutes...";
+      break;
+    case AssetReportCreationState.ReportReady:
+      reportCreationProgressFeedbackText = "Report is ready.";
+      break;
+    case AssetReportCreationState.ErrorGeneratingReport:
+      reportCreationProgressFeedbackText =
+        "There was an error generating the asset report.";
+      break;
+    case AssetReportCreationState.ReportFetchInitiated:
+      reportCreationProgressFeedbackText = "Fetching report...";
+      break;
+    case AssetReportCreationState.ReportFetchCompleted:
+      reportCreationProgressFeedbackText = "Report fetch completed.";
+      break;
+    default:
+      break;
+  }
+
   return (
     <div>
       <div>
         <button
           onClick={async () => {
+            setAssetReportCreationState(
+              AssetReportCreationState.CreationRequestInitiated
+            );
             const assetReportCreateResponse = await createAssetReport([
               plaidItem.accessToken,
             ]);
+            setAssetReportCreationState(
+              AssetReportCreationState.CreationRequestConfirmed
+            );
+
             console.log(assetReportCreateResponse);
 
             setAssetReportCreateResponse(assetReportCreateResponse);
@@ -79,6 +134,7 @@ export default function AssetReportCreationButton({
           Create asset report
         </button>
       </div>
+      <p>{reportCreationProgressFeedbackText}</p>
     </div>
   );
 }
