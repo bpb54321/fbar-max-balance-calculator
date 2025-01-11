45087453474157f07cbbac6f7d5e4e769ffe3638
Brian Blosser <bpb54321@gmail.com>
2025-01-11 09:05:55 -0500

    Only load data from storage if it has not already been loaded.

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -10,13 +10,15 @@ export const useLocalStorage = <T>(
 
   // Restore data from local storage
   useEffect(() => {
-    const locallyStoredData = window.localStorage.getItem(storageKey);
-    if (locallyStoredData) {
-      const parsedData = JSON.parse(locallyStoredData);
-      stateUpdaterFunction(parsedData);
+    if (!isDataLoadedFromLocalStorage) {
+      const locallyStoredData = window.localStorage.getItem(storageKey);
+      if (locallyStoredData) {
+        const parsedData = JSON.parse(locallyStoredData);
+        stateUpdaterFunction(parsedData);
+      }
+      setIsDataLoadedFromLocalStorage(true);
     }
-    setIsDataLoadedFromLocalStorage(true);
-  }, [storageKey, stateUpdaterFunction]);
+  }, [storageKey, stateUpdaterFunction, isDataLoadedFromLocalStorage]);
 
   // Save data to local storage when its updated
   useEffect(() => {
