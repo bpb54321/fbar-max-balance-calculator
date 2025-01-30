90648345af617bb8b70a375911e0d1b55ff2e79c
Brian Blosser <bpb54321@gmail.com>
2025-01-29 19:47:04 -0500

    Add basic HTML for account selection.

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,26 @@
-export default async function SettingsPage() {
-  return <h1>Settings Page</h1>;
+"use client";
+
+const accounts = ["Account 1", "Account 2"];
+
+export default function SettingsPage() {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <h2>Accounts to Include in Analysis</h2>
+      <form>
+        {accounts.map((account) => (
+          <div key={account}>
+            <label htmlFor={account}>{account}</label>
+            <input
+              type="checkbox"
+              value={account}
+              // checked={selectedItems.includes(item)}
+              onChange={() => {}}
+              id={account}
+            />
+          </div>
+        ))}
+      </form>
+    </div>
+  );
 }
