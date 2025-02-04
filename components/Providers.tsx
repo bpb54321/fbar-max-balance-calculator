e8fcf3af1e6e0375283ca6efbae6f581d6e04bda
Brian Blosser <bpb54321@gmail.com>
2025-02-04 11:04:20 -0500

    Uninstall google generative ai and adobe react spectrum.

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,13 +1,8 @@
 "use client";
-import { defaultTheme, Provider } from "@adobe/react-spectrum";
 import { AccountsProvider } from "@/contexts/accountsContext";
 
 import React, { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return (
-    <Provider theme={defaultTheme}>
-      <AccountsProvider>{children}</AccountsProvider>
-    </Provider>
-  );
+  return <AccountsProvider>{children}</AccountsProvider>;
 }
--- deprecated/AccountList.tsx
+++ deprecated/AccountList.tsx
@@ -1,17 +1,17 @@
-import { useAccounts } from "@/contexts/accountsContext";
-import FileUploadButton from "./FileUploadButton";
+// import { useAccounts } from "@/contexts/accountsContext";
+// import FileUploadButton from "./FileUploadButton";
 
-export function AccountList() {
-  const accountState = useAccounts();
-  return (
-    <div>
-      <h2>Accounts</h2>
-      {accountState.accounts.map((account) => (
-        <div key={account.name}>
-          <h3>{account.name}</h3>
-          <FileUploadButton />
-        </div>
-      ))}
-    </div>
-  );
-}
+// export function AccountList() {
+//   const accountState = useAccounts();
+//   return (
+//     <div>
+//       <h2>Accounts</h2>
+//       {accountState.accounts.map((account) => (
+//         <div key={account.name}>
+//           <h3>{account.name}</h3>
+//           <FileUploadButton />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
