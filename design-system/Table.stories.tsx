8c7fe3528f49f92f08d153fc22d972f6f8720abe
Brian Blosser <bpb54321@gmail.com>
2025-02-04 10:55:54 -0500

    Install Storybook and create first draft of Table component.

---
+++ design-system/Table.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Table from "./Table";
+
+const meta: Meta<typeof Table> = {
+  title: "Table",
+  component: Table,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Table>;
+
+export const Primary: Story = {
+  args: {},
+};
---
+++ design-system/Table.tsx
@@ -0,0 +1,18 @@
+export default function Table() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Payee</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2024-01-12</td>
+          <td>Landlord</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
