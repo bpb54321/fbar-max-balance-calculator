cb3c7b253d704934fc38f57d08e842aab512b056
Brian Blosser <bpb54321@gmail.com>
2025-02-15 20:26:21 -0500

    Get Geist to work correctly in Storybook.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import localFont from "next/font/local";
 import type { Preview } from "@storybook/react";
 import "../css/output.css";
-
-const GeistSans = localFont({
-  src: "../fonts/geist-sans/Geist-Variable.woff2",
-  variable: "--font-geist-sans",
-});
 
 const preview: Preview = {
   decorators: [
     (Story) => (
-      <div className={`font-sans ${GeistSans.variable} antialiased`}>
+      <div className={`font-sans antialiased`}>
         <Story />
       </div>
     ),
