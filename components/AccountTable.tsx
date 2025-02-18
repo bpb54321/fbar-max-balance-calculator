f4103457ef95a3bd480afeac0d4e6e6362989227
Brian Blosser <bpb54321@gmail.com>
2025-02-18 17:25:33 -0500

    Remove example table from homepage.

--- components/AccountTable.tsx
+++ components/AccountTable.tsx
@@ -1,94 +1,14 @@
 "use client";
 
 import { useSelectedAccounts } from "@/contexts/accountsContext";
-import Table from "@/design-system/Table";
 import formatAmount from "@/formatters/formatAmount";
 import Link from "next/link";
-import { v4 as uuidv4 } from "uuid";
-
-// Sample data
-type RowKey = "invoice" | "status" | "method" | "amount";
-const rowKeys: RowKey[] = ["invoice", "status", "method", "amount"];
-const columnHeaders = ["Invoice", "Status", "Method", "Amount"];
-const rowData = [
-  {
-    id: uuidv4(),
-    invoice: "INV001",
-    status: "Paid",
-    method: "Credit Card",
-    amount: "$250.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV002",
-    status: "Pending",
-    method: "PayPal",
-    amount: "$150.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV003",
-    status: "Unpaid",
-    method: "Bank Transfer",
-    amount: "$350.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV004",
-    status: "Paid",
-    method: "Credit Card",
-    amount: "$450.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV005",
-    status: "Paid",
-    method: "PayPal",
-    amount: "$550.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV006",
-    status: "Pending",
-    method: "Bank Transfer",
-    amount: "$200.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV007",
-    status: "Unpaid",
-    method: "Credit Card",
-    amount: "$300.00",
-  },
-];
-const footerData = [
-  {
-    id: "total",
-    value: "Total",
-    colSpan: 3,
-  },
-  {
-    id: "totalAmount",
-    value: "$2,500.00",
-    colSpan: 1,
-  },
-];
-const caption = "A list of your recent invoices.";
 
 export default function AccountTable() {
   const selectedAccounts = useSelectedAccounts();
 
   return (
     <div>
-      <div className="w-[590px]">
-        <Table
-          columnHeaders={columnHeaders}
-          rowKeys={rowKeys}
-          rowData={rowData}
-          footerData={footerData}
-          caption={caption}
-        />
-      </div>
       <table>
         <thead>
           <tr>
