62d6dce98cd1383068430a935eb706bcf5541803
Brian Blosser <bpb54321@gmail.com>
2025-04-11 06:40:41 -0400

    Add npm script for design-system tests.

--- design-system/table.visual-tests.spec.ts
+++ design-system/table.visual-tests.spec.ts
@@ -12,29 +12,4 @@ test.describe("Table", () => {
 
     await expect(table).toHaveScreenshot();
   });
-
-  // // Use example table loaded in Next.js so that font loading technique is identical
-  // test("has correct visual appearance", async ({ page }) => {
-  //   await page.goto("http://localhost:3000/");
-
-  //   const table = await page.getByTestId("table");
-
-  //   await expect(table).toHaveScreenshot();
-  // });
 });
-
-// test.describe("Shadcn table", () => {
-//   test("can get screenshot", async ({ page }) => {
-//     await page.goto("https://ui.shadcn.com/docs/components/table");
-
-//     await page.getByRole("combobox").click();
-
-//     await page.getByText("Default").click();
-
-//     await page.getByText("Loading...").waitFor({ state: "hidden" });
-
-//     const table = await page.locator("css=table");
-
-//     await expect(table).toHaveScreenshot();
-//   });
-// });
