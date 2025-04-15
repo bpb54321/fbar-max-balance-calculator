41ff65a7e0010f03c651d401bd2e84a67a35a9f1
Brian Blosser <bpb54321@gmail.com>
2025-04-15 16:52:17 -0400

    Integrate design system table into AccountTable.

--- components/AccountTable.tsx
+++ components/AccountTable.tsx
@@ -1,126 +1,66 @@
 "use client";
 
 import { useSelectedAccounts } from "@/contexts/accountsContext";
-import TablePackage from "@/design-system/table/TablePackage";
+import Caption from "@/design-system/table/Caption";
+import Table from "@/design-system/table/Table";
+import TableHeader from "@/design-system/table/TableHeader";
+import TableRow from "@/design-system/table/TableRow";
+import TableHeaderCell from "@/design-system/table/TableHeaderCell";
 import formatAmount from "@/formatters/formatAmount";
 import Link from "next/link";
 import { v4 as uuidv4 } from "uuid";
-
-// Sample data
-type RowKey = "invoice" | "status" | "method" | "amount";
-const rowKeys: RowKey[] = ["invoice", "status", "method", "amount"];
-const columnHeaders = ["Invoice", "Status", "Method", "Amount"];
-const rowData = [
-  {
-    id: uuidv4(),
-    invoice: "INV001",
-    status: "Paid",
-    method: "Credit Card",
-    amount: "$250.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV002",
-    status: "Pending",
-    method: "PayPal",
-    amount: "$150.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV003",
-    status: "Unpaid",
-    method: "Bank Transfer",
-    amount: "$350.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV004",
-    status: "Paid",
-    method: "Credit Card",
-    amount: "$450.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV005",
-    status: "Paid",
-    method: "PayPal",
-    amount: "$550.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV006",
-    status: "Pending",
-    method: "Bank Transfer",
-    amount: "$200.00",
-  },
-  {
-    id: uuidv4(),
-    invoice: "INV007",
-    status: "Unpaid",
-    method: "Credit Card",
-    amount: "$300.00",
-  },
-];
-const footerData = [
-  {
-    id: "total",
-    value: "Total",
-    colSpan: 3,
-  },
-  {
-    id: "totalAmount",
-    value: "$2,500.00",
-    colSpan: 1,
-  },
-];
-const caption = "A list of your recent invoices.";
+import { FontWeight, TextAlignment } from "@/design-system/table/enums";
+import TableBody from "@/design-system/table/TableBody";
+import TableBodyCell from "@/design-system/table/TableBodyCell";
 
 export default function AccountTable() {
   const selectedAccounts = useSelectedAccounts();
 
   return (
-    <div>
-      <div className="w-[590px]">
-        <TablePackage
-          columnHeaders={columnHeaders}
-          rowKeys={rowKeys}
-          rowData={rowData}
-          footerData={footerData}
-          caption={caption}
-        />
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Account Name</th>
-            <th>Max Balance 2022</th>
-            <th>Max Balance 2023</th>
-            <th>Max Balance 2024</th>
-            <th>Max Balance 2025</th>
-          </tr>
-        </thead>
-        <tbody>
+    <div className="w-[590px]">
+      <Table>
+        <Caption>Accounts</Caption>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell key="column-header-account-name">
+              Account Name
+            </TableHeaderCell>
+            <TableHeaderCell key="column-header-max-balance-2022">
+              Max Balance 2022
+            </TableHeaderCell>
+            <TableHeaderCell key="column-header-max-balance-2023">
+              Max Balance 2023
+            </TableHeaderCell>
+            <TableHeaderCell key="column-header-max-balance-2024">
+              Max Balance 2024
+            </TableHeaderCell>
+            <TableHeaderCell key="column-header-max-balance-2025">
+              Max Balance 2025
+            </TableHeaderCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
           {selectedAccounts.map((account) => (
-            <tr key={account.id}>
-              <td>
+            <TableRow key={account.id}>
+              <TableBodyCell fontWeight={FontWeight.Medium}>
                 <Link href={`/account/${account.id}`}>{account.name}</Link>
-              </td>
-              <td>
+              </TableBodyCell>
+              <TableBodyCell>
                 {formatAmount(account.maxBalancesByYear?.["2022"]?.balance)}
-              </td>
-              <td>
+              </TableBodyCell>
+              <TableBodyCell>
                 {formatAmount(account.maxBalancesByYear?.["2023"]?.balance)}
-              </td>
-              <td>
+              </TableBodyCell>
+              <TableBodyCell>
                 {formatAmount(account.maxBalancesByYear?.["2024"]?.balance)}
-              </td>
-              <td>
+              </TableBodyCell>
+              <TableBodyCell>
                 {formatAmount(account.maxBalancesByYear?.["2025"]?.balance)}
-              </td>
-            </tr>
+              </TableBodyCell>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
     </div>
   );
 }
