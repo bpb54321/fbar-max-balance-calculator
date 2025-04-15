e30ce44e77a65ca4f553f13b2348c761c916ef1a
Brian Blosser <bpb54321@gmail.com>
2025-04-15 17:01:16 -0400

    Convert the Max Balances by Year table to use the design system table.

--- components/TransactionTable.tsx
+++ components/TransactionTable.tsx
@@ -8,6 +8,15 @@ import {
   useAccountsDispatch,
 } from "@/contexts/accountsContext";
 import getTransactionsForAccount from "@/server-functions/getTransactionsForAccount";
+import Table from "@/design-system/table/Table";
+
+import TableHeader from "@/design-system/table/TableHeader";
+import TableRow from "@/design-system/table/TableRow";
+import TableHeaderCell from "@/design-system/table/TableHeaderCell";
+import TableBody from "@/design-system/table/TableBody";
+import TableBodyCell from "@/design-system/table/TableBodyCell";
+import Caption from "@/design-system/table/Caption";
+import { FontWeight, TextAlignment } from "@/design-system/table/enums";
 
 export default function TransactionTable({ accountId }: { accountId: string }) {
   const account = useAccount(accountId);
@@ -44,35 +53,48 @@ export default function TransactionTable({ accountId }: { accountId: string }) {
   return (
     <div>
       <button onClick={handleClick}>Reload transactions</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Payee</th>
-            <th>Memo</th>
-            <th>Amount</th>
-            <th>Balance</th>
-            <th>Year of Max Balance</th>
-          </tr>
-        </thead>
-        <tbody>
+
+      <Table>
+        <Caption>Max Balances by Year</Caption>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell key="column-header-date">Date</TableHeaderCell>
+            <TableHeaderCell key="column-header-payee">Payee</TableHeaderCell>
+            <TableHeaderCell key="column-header-memo">Memo</TableHeaderCell>
+            <TableHeaderCell key="column-header-amount">Amount</TableHeaderCell>
+            <TableHeaderCell key="column-header-balance">
+              Balance
+            </TableHeaderCell>
+            <TableHeaderCell key="column-header-year">
+              Year of Max Balance
+            </TableHeaderCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
           {Object.values(maxBalancesByYear).map((maxBalance) => {
             const transactionWithBalance = transactionsWithBalances.find(
               (transaction) => transaction.id === maxBalance.transactionId
             ) ?? { date: "", payeeName: "", memo: "", amount: 0, balance: 0 };
             return (
-              <tr key={maxBalance.id}>
-                <td>{transactionWithBalance.date}</td>
-                <td>{transactionWithBalance.payeeName}</td>
-                <td>{transactionWithBalance.memo}</td>
-                <td>{transactionWithBalance.amount / 1000}</td>
-                <td>{transactionWithBalance.balance / 1000}</td>
-                <td>{maxBalance.year}</td>
-              </tr>
+              <TableRow key={maxBalance.id}>
+                <TableBodyCell>{transactionWithBalance.date}</TableBodyCell>
+                <TableBodyCell>
+                  {transactionWithBalance.payeeName}
+                </TableBodyCell>
+                <TableBodyCell>{transactionWithBalance.memo}</TableBodyCell>
+                <TableBodyCell textAlignment={TextAlignment.Right}>
+                  {transactionWithBalance.amount / 1000}
+                </TableBodyCell>
+                <TableBodyCell textAlignment={TextAlignment.Right}>
+                  {transactionWithBalance.balance / 1000}
+                </TableBodyCell>
+                <TableBodyCell>{maxBalance.year}</TableBodyCell>
+              </TableRow>
             );
           })}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
+
       <table>
         <thead>
           <tr>
