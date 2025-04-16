15294eab511743457963197c4f9fc10e15312812
Brian Blosser <bpb54321@gmail.com>
2025-04-16 08:34:10 -0400

    Remove import of font definition.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
 
-// Manually import the CSS file which imports the Geist Sans font definition
-// file from node modules because Next.js font loader doesn't seem to
-// work in Storybook.
-import "./fonts.css";
-
 import "../css/output.css";
 
 const preview: Preview = {
