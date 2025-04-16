4b1abb11e801daf86fdc0d2e1f33fccdb67f79aa
Brian Blosser <bpb54321@gmail.com>
2025-04-16 15:45:13 -0400

    Add cursor-pointer to the button.

--- design-system/button/Button.tsx
+++ design-system/button/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({ children, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="inline-flex items-center justify-center font-medium text-sm py-2 px-4 bg-primary text-primary-foreground rounded-md"
+      className="inline-flex items-center justify-center font-medium text-sm py-2 px-4 bg-primary text-primary-foreground rounded-md cursor-pointer"
     >
       {children}
     </button>
