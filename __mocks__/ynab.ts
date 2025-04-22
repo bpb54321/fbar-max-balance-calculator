5c5d67f31097d3f96fe5f24f54fb6ab8efde4ab0
Brian Blosser <bpb54321@gmail.com>
2025-04-22 06:02:10 -0400

    Type the mock prototype.

--- __mocks__/ynab.ts
+++ __mocks__/ynab.ts
@@ -17,7 +17,10 @@ export const mockAccounts: YnabAccount[] = [
 
 export const mockGetAccounts = vi.fn();
 
-export const api = vi.fn(function (accessToken: string) {
+export const api = vi.fn(function (
+  this: { accounts: { getAccounts: typeof mockGetAccounts } },
+  accessToken: string
+) {
   this.accounts = {
     getAccounts: mockGetAccounts,
   };
