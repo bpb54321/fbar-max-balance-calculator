f1ce47defb3f4c797e957c06cef27469802191e6
Brian Blosser <bpb54321@gmail.com>
2025-04-22 14:06:27 -0400

    Update getAccounts to use the YnabService again.

--- server-functions/getAccounts.ts
+++ server-functions/getAccounts.ts
@@ -1,8 +1,11 @@
 import { TokenManager } from "@/services/tokenManager";
+import YnabService from "@/ynab-service/ynabService";
 import * as ynab from "ynab";
 
 export default async function getAccounts() {
   const ynabApi = new ynab.api(TokenManager.getToken());
+
+  const ynabService = new YnabService(TokenManager.getToken());
 
   const budgets = await ynabApi.budgets.getBudgets(true);
   const defaultBudgetId = budgets.data.default_budget?.id;
@@ -11,7 +14,7 @@ export default async function getAccounts() {
     throw new Error("Default budget id not defined for user");
   }
 
-  const accounts = await ynabApi.accounts.getAccounts(defaultBudgetId);
+  const accounts = await ynabService.getAccounts(defaultBudgetId);
 
-  return accounts.data.accounts;
+  return accounts;
 }
