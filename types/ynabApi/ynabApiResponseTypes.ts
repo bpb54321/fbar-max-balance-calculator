a85fda51e2d52448b5410898e4fa56a3947814df
Brian Blosser <bpb54321@gmail.com>
2025-04-26 18:21:11 -0400

    Add todo.

--- types/ynabApi/ynabApiResponseTypes.ts
+++ types/ynabApi/ynabApiResponseTypes.ts
@@ -1,6 +1,8 @@
 import { YnabAccount } from "./YnabAccount";
 import { YnabTransaction } from "./YnabTransaction";
 
+// TODO: Delete these types which are now provided by the ynab sdk
+
 export interface YnabAccountsResponse {
   data: {
     accounts: YnabAccount[];
