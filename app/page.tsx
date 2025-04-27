152b469425a6fe6bdba1e4e4cf1cd9de26b295df
Brian Blosser <bpb54321@gmail.com>
2025-04-27 09:22:17 -0400

    Display message if default budget id not able to be fetched due to expired token.

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,10 @@ export default function Home() {
   const ynabAuthorizationUrl = `https://app.ynab.com/oauth/authorize?client_id=${process.env["YNAB_OATH_CLIENT_ID"]}&redirect_uri=${process.env["YNAB_OATH_REDIRECT_URI"]}&response_type=token`;
   return (
     <div>
-      <>
-        {/* Components that run side effects. */}
-        <YnabAuthorization />
-        <DefaultBudgetIdFetcher />
-      </>
+      <YnabAuthorization />
       <MainNavigation />
       <a href={ynabAuthorizationUrl}>Authorize With YNAB</a>
+      <DefaultBudgetIdFetcher />
       <Accounts />
     </div>
   );
--- components/DefaultBudgetIdFetcher.tsx
+++ components/DefaultBudgetIdFetcher.tsx
@@ -2,20 +2,43 @@
 
 import { BudgetActionTypes, useBudgetDispatch } from "@/contexts/budgetContext";
 import getDefaultBudgetId from "@/utility-functions/getDefaultBudgetId";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface YnabError {
+  error: {
+    id: string;
+    name: string;
+    detail: string;
+  };
+}
 
 export default function DefaultBudgetIdFetcher() {
-  // TODO: Move ynab token into Context so we can react to changes with it
+  const [invalidTokenError, setInvalidTokenError] = useState<YnabError | null>(
+    null
+  );
   const budgetDispatch = useBudgetDispatch();
   useEffect(() => {
     const updateBudgetId = async () => {
-      const defaultBudgetId = await getDefaultBudgetId();
-      budgetDispatch({
-        type: BudgetActionTypes.DefaultBudgetIdSet,
-        defaultBudgetId,
-      });
+      try {
+        const defaultBudgetId = await getDefaultBudgetId();
+        budgetDispatch({
+          type: BudgetActionTypes.DefaultBudgetIdSet,
+          defaultBudgetId,
+        });
+      } catch (e) {
+        setInvalidTokenError(e as YnabError);
+      }
     };
     updateBudgetId();
   }, []);
+
+  if (invalidTokenError) {
+    return (
+      <p className="mt-2 mb-2 text-red-600">
+        There was an error retrieving information from YNAB. Please click on the
+        preceding link to reauthorize the connection to YNAB.
+      </p>
+    );
+  }
   return null;
 }
