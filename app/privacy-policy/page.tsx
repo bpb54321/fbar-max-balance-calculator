24b4616c44401c13414b0705dd7405e7998e39a2
Brian Blosser <bpb54321@gmail.com>
2025-04-30 06:36:17 -0400

    Use Paragraph component on privacy policy page.

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,24 +1,25 @@
 import Heading1 from "@/design-system/headings/heading1/Heading1";
 import Heading2 from "@/design-system/headings/heading2/Heading2";
+import Paragraph from "@/design-system/paragraph/Paragraph";
 
 export default function PrivacyPolicy() {
   return (
     <>
       <Heading1>Privacy Policy</Heading1>
       <Heading2>How your data will be handled, stored, secured</Heading2>
-      <p>
+      <Paragraph>
         All data obtained via the YNAB API will be stored in local storage on
         your browser.
-      </p>
-      <p>
+      </Paragraph>
+      <Paragraph>
         You can delete the data stored on your browser at any time by clearing
         the local storage of your browser for the application's hostname.
-      </p>
+      </Paragraph>
       <Heading2>Third parties</Heading2>
-      <p>
+      <Paragraph>
         We guarantee that the data obtained through the YNAB API will not be
         passed to any third party.
-      </p>
+      </Paragraph>
     </>
   );
 }
