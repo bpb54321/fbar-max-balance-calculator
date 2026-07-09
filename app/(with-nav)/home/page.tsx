ea31cb5d4f6422160e7960d2443a5cd3fe5d1b27
Brian Blosser <bpb54321@gmail.com>
2026-07-09 05:28:37 -0400

    Move the default budget id fetcher to the layout of (with-nav).

--- app/(with-nav)/home/page.tsx
+++ app/(with-nav)/home/page.tsx
@@ -1,11 +1,9 @@
 import Accounts from "@/components/Accounts";
-import DefaultBudgetIdFetcher from "@/components/DefaultBudgetIdFetcher";
 import Disclaimer from "@/components/Disclaimer";
 
 export default function Home() {
   return (
     <div>
-      <DefaultBudgetIdFetcher />
       <Accounts />
       <Disclaimer />
     </div>
