3b79c623e444ce91de54f3da1e0d236b2e2a6650
Brian Blosser <bpb54321@gmail.com>
2026-07-09 05:28:46 -0400

    Update error message.

--- components/DefaultBudgetIdFetcher.tsx
+++ components/DefaultBudgetIdFetcher.tsx
@@ -28,12 +28,10 @@ export default function DefaultBudgetIdFetcher() {
     updateBudgetId();
   }, [budgetDispatch]);
 
-  // TODO: Update this error text.
   if (hasError) {
     return (
       <p className="mt-2 mb-2 text-red-600">
-        There was an error retrieving information from YNAB. Please click on the
-        preceding link to reauthorize the connection to YNAB.
+        There was an error retrieving the default plan id.
       </p>
     );
   }
